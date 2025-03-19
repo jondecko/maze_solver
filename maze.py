e770ba94bcd215d8a1d28b34cf528c241083b3a8
jondecko <jondecko@gmail.com>
2025-03-19 02:29:19 +0000

    maze solving

--- maze.py
+++ maze.py
@@ -23,6 +23,7 @@ class Maze():
         self._break_entrance_and_exit()
         self._break_walls_r(0, 0)
         self._draw_cells()
+        self._reset_cells_visited()
 
 
     def _create_cells(self):
@@ -49,11 +50,11 @@ class Maze():
         self._animate()
 
 
-    def _animate(self):
+    def _animate(self, sleep_period=0.01):
         if self._win is None:
             return
         self._win.redraw()
-        time.sleep(0.01)
+        time.sleep(sleep_period)
 
 
     def _break_entrance_and_exit(self):
@@ -107,3 +108,65 @@ class Maze():
 
             self._break_walls_r(next_index[0], next_index[1])
            
+
+    def _reset_cells_visited(self):
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._cells[i][j].visited = False
+
+
+    def solve(self):
+        return self._solve_r(0, 0)
+
+
+    def _solve_r(self, i, j):
+        self._animate(0.05)
+        self._cells[i][j].visited = True
+
+        if i+1 == self._num_cols and j+1 == self._num_rows:
+            return True
+
+        directions = ['up', 'right', 'down', 'left']
+        for direction in directions:
+            #If there is a cell in that direction,
+            #there is no wall blocking you,
+            #and that cell hasn't been visited
+            cell1 = self._cells[i][j]
+            match direction:
+                case 'up':
+                    if j > 0:
+                        cell2 = self._cells[i][j-1]
+                        if not cell1.has_top_wall and not cell2.visited:
+                            cell1.draw_move(cell2)
+                            result = self._solve_r(i, j-1)
+                            if result:
+                                return result
+                            cell1.draw_move(cell2, True)
+                case 'right':
+                    if i+1 < self._num_cols:
+                        cell2 = self._cells[i+1][j]
+                        if not cell1.has_right_wall and not cell2.visited:
+                            cell1.draw_move(cell2)
+                            result = self._solve_r(i+1, j)
+                            if result:
+                                return result
+                            cell1.draw_move(cell2, True)
+                case 'down':
+                    if j+1 < self._num_rows:
+                        cell2 = self._cells[i][j+1]
+                        if not cell1.has_bottom_wall and not cell2.visited:
+                            cell1.draw_move(cell2)
+                            result = self._solve_r(i, j+1)
+                            if result:
+                                return result
+                            cell1.draw_move(cell2, True)
+                case 'left':
+                    if i > 0:
+                        cell2 = self._cells[i-1][j]
+                        if not cell1.has_left_wall and not cell2.visited:
+                            cell1.draw_move(cell2)
+                            result = self._solve_r(i-1, j)
+                            if result:
+                                return result
+                            cell1.draw_move(cell2, True)
+        return False
